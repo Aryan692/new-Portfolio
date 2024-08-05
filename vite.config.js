d1c3b1acf62c3bb1acbc720c8274b63d4c3a7d23
balram <taryan692@gmail.com>
2024-08-05 12:53:51 +0530

    base url added

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  base:"/new-Portfolio/",
+  base: "/new-Portfolio/",
  
   build:{
     outDir:"./docs"
